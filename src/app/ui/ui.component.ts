2496dca770ce1b3adc65a84f00f7588effdf7f31
Johan-Ortega811 <masterchief_2001118@hotmail.com>
2021-11-15 22:11:52 -0600

    added code

---
+++ src/app/ui/ui.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { formula } from '../formula/formula';
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.css']
+})
+export class UiComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+
+  value = 0;
+  resultado = 0;
+  calcular() {
+    let res = 0;
+    let form: formula = new formula();
+    res = form.calcular(this.value);
+    this.resultado = res;
+  }
+}
